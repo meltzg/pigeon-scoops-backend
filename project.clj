cd730e48101477058f4f069373b1baf2dd2248cd
Greg Meltzer <gregory.m.meltzer@gmail.com>
2024-11-02 22:49:57 -0400

    min lein version 2

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject pigeon-scoops-backend "0.1.0-SNAPSHOT"
   :description "Pigeon Scoops manager API"
   :url "http://api.pigeon-scoops.com/"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [ring "1.13.0"]
                  [integrant "0.13.1"]
