7698a49f44e8f40ff3e69341c23ae65572ca849c
Gregory Meltzer <gregory.m.meltzer@gmail.com>
2025-02-09 18:54:29 -0500

    remove unit cost from order item (#16)

--- src/pigeon_scoops_backend/user_order/responses.clj
+++ src/pigeon_scoops_backend/user_order/responses.clj
@@ -15,7 +15,6 @@
   {:order-item/id                   uuid?
    :order-item/recipe-id            uuid?
    :order-item/order-id             uuid?
-   :order-item/unit-cost            number?
    :order-item/status               (s/and keyword? status)
    (ds/opt :order-item/amount)      number?
    (ds/opt :order-item/amount-unit) (s/and keyword?
