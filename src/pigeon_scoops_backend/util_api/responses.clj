094372838e33abac18f03fa16742091c2ab2473e
Gregory Meltzer <gregory.m.meltzer@gmail.com>
2025-02-09 18:36:37 -0500

    add order status to constants route (#15)

--- src/pigeon_scoops_backend/util_api/responses.clj
+++ src/pigeon_scoops_backend/util_api/responses.clj
@@ -4,13 +4,15 @@
             [pigeon-scoops-backend.grocery.responses :refer [departments]]
             [pigeon-scoops-backend.units.common :as common]
             [pigeon-scoops-backend.units.mass :as mass]
-            [pigeon-scoops-backend.units.volume :as volume]))
+            [pigeon-scoops-backend.units.volume :as volume]
+            [pigeon-scoops-backend.user-order.responses :refer [status]]))
 
 (def constants
-  {:constants/unit-types  [(s/and keyword?
-                                  (set (concat common/other-units
-                                               (keys mass/conversion-map)
-                                               (keys volume/conversion-map))))]
-   :constants/departments [(s/and keyword? departments)]
-   :constants/roles       [(s/and keyword? roles)]})
+  {:constants/unit-types     [(s/and keyword?
+                                     (set (concat common/other-units
+                                                  (keys mass/conversion-map)
+                                                  (keys volume/conversion-map))))]
+   :constants/departments    [(s/and keyword? departments)]
+   :constants/roles          [(s/and keyword? roles)]
+   :constants/order-statuses [(s/and keyword? status)]})
 
