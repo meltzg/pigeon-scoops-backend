7e40bce996b4431994d4bad97648c4404ddfbc1d
Greg Meltzer <gregory.m.meltzer@gmail.com>
2025-02-26 00:49:48 -0500

    adding function to get the minimal grocery unit for an amount

--- src/pigeon_scoops_backend/grocery/utils.clj
+++ src/pigeon_scoops_backend/grocery/utils.clj
@@ -1,14 +1,23 @@
 (ns pigeon-scoops-backend.grocery.utils
   (:require [pigeon-scoops-backend.units.common :as common]))
 
-(defn get-grocery-unit-for-amount [{:grocery/keys [units]} amount amount-unit]
-  (let [[unit-key unit-type-key] (->> amount-unit
+(defn get-minimal-unit [units amount amount-unit]
+  (let [[quantity-key unit-key] (->> amount-unit
                                      (namespace)
                                      (keyword)
-                                     (get {:mass [:grocery-unit/unit-mass :grocery-unit/unit-mass-type]
-                                           :volume[:grocery-unit/unit-volume :grocery-unit/unit-volume-type]
-                                           :common [:grocery-unit/unit-common :grocery-unit/unit-common-type]}))]
-    [unit-key unit-type-key]))
+                                     (get {:mass   [:grocery-unit/unit-mass :grocery-unit/unit-mass-type]
+                                           :volume [:grocery-unit/unit-volume :grocery-unit/unit-volume-type]
+                                           :common [:grocery-unit/unit-common :grocery-unit/unit-common-type]}))
+        comparable-amount (common/to-comparable amount amount-unit)]
+    (->> units
+         (map #(assoc % :comparable-amount (common/to-comparable (quantity-key %) (unit-key %))))
+         (sort-by :comparable-amount)
+         (filter #(and (some? (common/convert amount amount-unit (unit-key %)))
+                       (<= comparable-amount (:comparable-amount %))))
+         (first))))
+
+(defn get-quantized-units [])
 
 (comment
-  (get-grocery-unit-for-amount [] 4 :common/unit))
+  (get-minimal-unit [{:grocery-unit/unit-mass 5 :grocery-unit/unit-mass-type :mass/kg}
+                     {:grocery-unit/unit-mass 5 :grocery-unit/unit-mass-type :mass/g}] 4 :mass/lb))
