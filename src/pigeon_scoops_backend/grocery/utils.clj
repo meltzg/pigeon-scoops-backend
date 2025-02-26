d84e649b1364fcd262fd4ab6cdfcb56dcf1e4bb4
Greg Meltzer <gregory.m.meltzer@gmail.com>
2025-02-26 17:11:31 -0500

    adding function to get quantized units

--- src/pigeon_scoops_backend/grocery/utils.clj
+++ src/pigeon_scoops_backend/grocery/utils.clj
@@ -1,23 +1,41 @@
 (ns pigeon-scoops-backend.grocery.utils
   (:require [pigeon-scoops-backend.units.common :as common]))
 
-(defn get-minimal-unit [units amount amount-unit]
-  (let [[quantity-key unit-key] (->> amount-unit
-                                     (namespace)
-                                     (keyword)
-                                     (get {:mass   [:grocery-unit/unit-mass :grocery-unit/unit-mass-type]
-                                           :volume [:grocery-unit/unit-volume :grocery-unit/unit-volume-type]
-                                           :common [:grocery-unit/unit-common :grocery-unit/unit-common-type]}))
+(defn get-unit-keys [amount-unit]
+  (->> amount-unit
+       (namespace)
+       (keyword)
+       (get {:mass   [:grocery-unit/unit-mass :grocery-unit/unit-mass-type]
+             :volume [:grocery-unit/unit-volume :grocery-unit/unit-volume-type]
+             :common [:grocery-unit/unit-common :grocery-unit/unit-common-type]})))
+
+(defn get-optimal-unit [units amount amount-unit]
+  (let [[quantity-key unit-key] (get-unit-keys amount-unit)
         comparable-amount (common/to-comparable amount amount-unit)]
     (->> units
          (map #(assoc % :comparable-amount (common/to-comparable (quantity-key %) (unit-key %))))
-         (sort-by :comparable-amount)
-         (filter #(and (some? (common/convert amount amount-unit (unit-key %)))
-                       (<= comparable-amount (:comparable-amount %))))
+         (filter (comp some? :comparable-amount))
+         (sort-by #(Math/abs ^Double (- comparable-amount (:comparable-amount %))))
          (first))))
 
-(defn get-quantized-units [])
+(defn get-quantized-units [units amount amount-unit]
+  (let [[quantity-key unit-key] (get-unit-keys amount-unit)
+        needed-units (loop [needed-units []
+                            remaining-amount amount]
+                       (let [unit (get-optimal-unit units remaining-amount amount-unit)
+                             remaining-amount (if unit
+                                                (first (common/add-amounts remaining-amount amount-unit
+                                                                           (- (quantity-key unit)) (unit-key unit)))
+                                                remaining-amount)]
+                         (cond (nil? unit) needed-units
+                               (<= remaining-amount 0) (conj needed-units unit)
+                               :else (recur (conj needed-units unit) remaining-amount))))]
+    (->> needed-units
+         (group-by identity)
+         (vals)
+         (map (fn [group]
+                (assoc (first group) :grocery-unit/quantity (count group)))))))
 
 (comment
-  (get-minimal-unit [{:grocery-unit/unit-mass 5 :grocery-unit/unit-mass-type :mass/kg}
-                     {:grocery-unit/unit-mass 5 :grocery-unit/unit-mass-type :mass/g}] 4 :mass/lb))
+  (get-optimal-unit [{:grocery-unit/unit-mass 5 :grocery-unit/unit-mass-type :mass/kg}
+                     {:grocery-unit/unit-mass 5 :grocery-unit/unit-mass-type :mass/g}] 5100 :volume/gal))
--- test/pigeon_scoops_backend/grocery/utils_test.clj
+++ test/pigeon_scoops_backend/grocery/utils_test.clj
@@ -1,2 +1,38 @@
 (ns pigeon-scoops-backend.grocery.utils-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [pigeon-scoops-backend.grocery.utils :as utils]))
+
+(def units [{:grocery-unit/unit-mass 5
+             :grocery-unit/unit-mass-type :mass/kg
+             :grocery-unit/unit-cost 50}
+            {:grocery-unit/unit-mass 5
+             :grocery-unit/unit-mass-type :mass/g
+             :grocery-unit/unit-cost 0.05}])
+
+(deftest get-optimal-unit-test
+  (testing "given a set of units and a needed amount, retrieve the unit closest in amount"
+    (are [amount amount-unit expected]
+      (= (dissoc (utils/get-optimal-unit units amount amount-unit) :comparable-amount)
+         expected)
+      2600 :mass/g {:grocery-unit/unit-mass 5
+                    :grocery-unit/unit-mass-type :mass/kg
+                    :grocery-unit/unit-cost 50}
+      100 :mass/g {:grocery-unit/unit-mass 5
+                   :grocery-unit/unit-mass-type :mass/g
+                   :grocery-unit/unit-cost 0.05}
+      100 :volume/gal nil)))
+
+(deftest get-quantized-units-test
+  (testing "given a set of units and a needed amount, we can get a list of the necessary units and their quantities"
+    (are [amount amount-unit expected]
+      (= (map #(select-keys % (keys (first expected))) (utils/get-quantized-units units amount amount-unit))
+         expected)
+      5105 :mass/g [{:grocery-unit/unit-mass 5
+                     :grocery-unit/unit-mass-type :mass/kg
+                     :grocery-unit/unit-cost 50
+                     :grocery-unit/quantity 1}
+                    {:grocery-unit/unit-mass 5
+                     :grocery-unit/unit-mass-type :mass/g
+                     :grocery-unit/unit-cost 0.05
+                     :grocery-unit/quantity 21}]
+      100 :volume/gal [])))
